07357eda739e352c2b23821a2da376eef84f222a
ShinpeiH <dctechsf@gmail.com>
2023-07-18 23:45:40 -0700

    continued working on exercise 16

--- ruby_book/exercises_ex_16_pt_2.rb
+++ ruby_book/exercises_ex_16_pt_2.rb
@@ -8,18 +8,22 @@ count = 0
 count_2 = 0
 
 loop do
-  name = contacts.keys.shift
+  name = contacts.keys.shift # splitting shift to another variable wont fix not accessing "Sally"
+ 
+  p name  
   loop do
     contacts[name][info.shift] = contact_data[count_2].shift # count_2 part should be right
     count += 1
-    p contacts
-    break if count >= 2
-  end
+    
+    break if count >= 2 # == 3 causes terminal to freeze
+  end                   # >= 3 will include phone number but also include nil=>"sally@
+  p count_2             # >= 2 will put "sally@ as :phone
   count_2 += 1
-  break if count_2 >= 1
+  break if count_2 >= 2
 end
 
 p contacts
+
 
 
 
