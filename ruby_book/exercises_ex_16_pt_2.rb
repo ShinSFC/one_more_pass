7bb8f7a8e45aae8bd7b91050e707cf8cc7a28f66
ShinpeiH <dctechsf@gmail.com>
2023-07-31 20:59:24 -0700

    working in ex 16

--- ruby_book/exercises_ex_16_pt_2.rb
+++ ruby_book/exercises_ex_16_pt_2.rb
@@ -1,25 +1,29 @@
-contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
-  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contact_data = [
+  ["joe@email.com", "123 Main st.", "555-123-4567"],
+  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]
+]
 
 contacts = { "Joe Smith" => {}, "Sally Johnson" => {} }
 
 info = [:email, :address, :phone]
 count = 0
-count_2 = 0
+contact_data_count = 0
+
 
 loop do
-  name = contacts.keys.shift # splitting shift to another variable wont fix not accessing "Sally"
- 
+  name_array = contacts.keys # splitting shift to another variable wont fix not accessing "Sally"
+  name = name_array.shift
+  p contacts.keys
   p name  
   loop do
-    contacts[name][info.shift] = contact_data[count_2].shift # count_2 part should be right
+    contacts[name][info.shift] = contact_data[contact_data_count].shift # count_2 part should be right
     count += 1
     
     break if count >= 2 # == 3 causes terminal to freeze
   end                   # >= 3 will include phone number but also include nil=>"sally@
-  p count_2             # >= 2 will put "sally@ as :phone
-  count_2 += 1
-  break if count_2 >= 2
+  p contact_data_count            # >= 2 will put "sally@ as :phone
+  contact_data_count += 1
+  break if contact_data_count >= 2
 end
 
 p contacts
