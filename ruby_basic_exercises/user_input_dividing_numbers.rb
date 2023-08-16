79fa6111a059a2cfdd986cc68ab521c84b1287ef
ShinpeiH <dctechsf@gmail.com>
2023-08-15 20:26:32 -0700

    added 2 user input

--- ruby_basic_exercises/user_input_dividing_numbers.rb
+++ ruby_basic_exercises/user_input_dividing_numbers.rb
@@ -7,11 +7,27 @@ denominator = 0
 
 loop do
   puts '>> Please enter the numerator:'
-  numerator = gets
+  numerator = gets.chomp
+  break if valid_number?(numerator) == true
+  puts 'Invalid input. Only integers allowed.'
 end
 
 loop do
   puts '>> Please enter the denominator:'
-  denominator = gets
+  denominator = gets.chomp
+  
+  if denominator == '0'
+    puts '>> Invalid input. A denominator of 0 is not allowed.'
+  elsif valid_number?(denominator) == true
+    break
+  else
+    puts 'Invalid input. Only integers allowed.'
+  end
 end
+
+answer = numerator.to_i / denominator.to_i
+
+puts ">> #{numerator} / #{denominator} is #{answer}"
+
+
 
