b008ade9a424daa201b9f3da719c7b8a27c74d0e
ShinpeiH <dctechsf@gmail.com>
2023-08-18 00:19:23 -0700

    finished opposites attract and started return

--- ruby_basic_exercises/user_input_opposites_attract.rb
+++ ruby_basic_exercises/user_input_opposites_attract.rb
@@ -4,30 +4,30 @@ end
 
 # p valid_number?('g')
 
-# above method is verifies for non-zero integer
-
 num1 = 0
 num2 = 0
 
-
-loop do
-  puts '>> Please enter a positive or negative integer:'
-  num1 = gets.chomp
-  
-  puts '>> Please enter a positive or negative integer:'
-  num2 = gets.chomp
- 
-  if 
-    puts '>> Invalid input. Only non-zero integers are allowed.'
-  elsif
-    puts '>> Sorry. One integer must be positive, one must be negative.'
-    puts '>> Please start over.'
-  else
-    break
+def get_number
+  loop do
+    puts '>> Please enter a positive or negative integer:'
+    num = gets.chomp
+    return num if valid_number?(num) == true
+    puts '>> Invalid input. Only non-zero integers are allowed.' 
   end
 end
 
+loop do
+  num1 = get_number.to_i
+  num2 = get_number.to_i
 
-p num1
-p num2
+  break if num1 * num2 < 0
+  puts '>> Sorry. One integer must be positive, one must be negative.'
+  puts '>> Please start over.'
+end
+
+answer = num1 + num2
+
+p answer
+
+puts "#{num1} + #{num2} = #{answer}"
   
